97f7bf792fe9e6aa7137bd0a9f23197977e80440
Sanye1991 <52860510+Sanye1991@users.noreply.github.com>
2022-05-17 00:00:15 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,6 +4,6 @@
 
 print('This is test1')
 print('now i add 1 more sentence')
-print('now i edit my python code from github)
+print('now i edit my python code from github')
 if __name__ == '__main__':
     pass
